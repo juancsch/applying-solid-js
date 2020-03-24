9be53419acab39e4e7d21645c858bf65475f697a
Juan Carlos Serrano <juancarlos.serrano.chicharro@bbvaitspain.com>
2020-03-24 16:10:42 +0100

    Extract method with side effects

--- src/main.js
+++ src/main.js
@@ -3,22 +3,34 @@ const fs = require('fs')
 
 main(process.argv[2])
 
-async function main (filepath) {
+async function main (location) {
 
     try {
-        const data = await fs.promises.readFile(path.resolve(filepath), 'utf8')
+        const data = await input(location)
 
         const wordCount = data.split(/\s/).length
         const wordCountJSON = {
             wordCount
         }
 
-        await fs.promises.writeFile(
-            path.resolve('word-count.json'),
-            JSON.stringify(wordCountJSON, null, 2)
-        )
+        await output(wordCountJSON)
     } catch (e) {
         console.error(e)
         process.exit(1)
     }
 }
+
+function input (filepath) {
+    const filepathResolved = path.resolve(filepath)
+    return fs.promises.readFile(filepathResolved, 'utf8')
+}
+
+const saveTo = filepath => content => {
+    const filepathResolved = path.resolve(filepath)
+    return fs.promises.writeFile(
+        filepathResolved,
+        JSON.stringify(content, null, 2)
+    )
+}
+
+const output = saveTo('word-count.json')
